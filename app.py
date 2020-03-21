1d1517da59414cac197e0d7258dcbc3be1447b0d
Pyleks <aleksander.andersen@outlook.ie>
2020-03-21 08:47:30 +0000

    Added img and customized bootstrap container

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT', '5001')),
+            port=int(os.environ.get('PORT', '5005')),
             debug=True)
