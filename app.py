8edaa48a6b6702e5a9f3e9335a16cb699a76c4d4
Pyleks <aleksander.andersen@outlook.ie>
2020-03-21 16:14:05 +0000

    Added food images to page

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
 import json
 from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'nordicPastry'
+app.config["MONGO_URI"] = 'mongodb+srv://myRoot:LoveMongoDB87@myamazingcluster-96wib.mongodb.net/nordicPastry?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def index():
-    return render_template("index.html")
+def recipes():
+    return render_template("index.html",
+        imageDB=mongo.db.imageDB.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
