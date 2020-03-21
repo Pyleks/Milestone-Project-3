60c2243f44c980b0ad7182dd25da89aa45f37538
Pyleks <aleksander.andersen@outlook.ie>
2020-03-21 07:37:21 +0000

    added basic app.py functionality

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+import json
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT', '5001')),
+            debug=True)
