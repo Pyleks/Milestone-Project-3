4aaf28e87bc4653d23f378a301966a51d944b24f
Pyleks <aleksander.andersen@outlook.ie>
2020-03-22 13:51:54 +0000

    Successfully connected the DB to the new pages

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import json
 from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'nordicPastry'
@@ -13,6 +14,13 @@ mongo = PyMongo(app)
 def recipes():
     return render_template("index.html",
         imageDB=mongo.db.imageDB.find())
+
+
+@app.route('/viewBake/<task_id>')
+def viewBake(task_id):
+    view_task = mongo.db.imageDB.find_one({"_id": ObjectId(task_id)})
+    return render_template('open-pastry.html', task=view_task)
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
