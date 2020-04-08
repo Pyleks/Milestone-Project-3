993e23e3881e895080bb5315d8e19a4663d9a3a5
Pyleks <aleksander.andersen@outlook.ie>
2020-04-08 07:49:20 +0100

    Added dynamic stars during star review

--- static/js/home.js
+++ static/js/home.js
@@ -6,265 +6,92 @@ document.getElementById("disable-overlay").addEventListener("click", function(){
     document.getElementById("overlay").style.display = "none";
 });
 
-// function myFunc() {
-//     console.log("This is a fking number", star_rate);
-//     if(star_rate >= 0 && star_rate <= 1) {
-//         goldenStarOne();
-//         emptyStarTwo();
-//         emptyStarThree();
-//         emptyStarFour();
-//         emptyStarFive();
-//
-//     }else if(star_rate >= 1 && star_rate <= 2) {
-//         goldenStarOne();
-//         goldenStarTwo();
-//         emptyStarThree();
-//         emptyStarFour();
-//         emptyStarFive();
-//
-//     }else if(star_rate >= 3 && star_rate <= 4){
-//         goldenStarOne();
-//         goldenStarTwo();
-//         goldenStarThree();
-//         emptyStarFour();
-//         emptyStarFive();
-//
-//     }else if(star_rate >= 4 && star_rate <= 5){
-//         goldenStarOne();
-//         goldenStarTwo();
-//         goldenStarThree();
-//         goldenStarFour();
-//         emptyStarFive();
-
-    // }else if(star_rate >= 4.6 && star_rate >= 5){
-    //     goldenStarOne();
-    //     goldenStarTwo();
-    //     goldenStarThree();
-    //     goldenStarFour();
-    //     goldenStarFive();
-    //
-    // } else {
-    //     emptyStarOne();
-    //     emptyStarTwo();
-    //     emptyStarThree();
-    //     emptyStarFour();
-    //     emptyStarFive();
-    // }
+let OneStar = document.getElementById('starRating_1');
+const TwoStar = document.getElementById('starRating_2');
+const ThreeStar = document.getElementById('starRating_3');
+const FourStar = document.getElementById('starRating_4');
+const FiveStar = document.getElementById('starRating_5');
+const starBox = document.getElementById('star-box');
 
 
+// One star
+OneStar.addEventListener('mouseover', function () {
+    OneStar.classList.add("fas", "fa-star");
+    TwoStar.classList.remove("fas", "fa-star");
+    TwoStar.classList.add("far", "fa-star");
+});
 
-// function goldenStarOne() {
-//     let element = document.getElementById("starRating_1");
-//     element.classList.add("fas");
-//     element.classList.add("fa-star");
-//     element.classList.add("bg-blue");
-// }
-//
-// function goldenStarTwo() {
-//     let element = document.getElementById("starRating_2");
-//     element.classList.add("fas");
-//     element.classList.add("fa-star");
-//     element.classList.add("bg-blue");
-// }
-// function goldenStarThree() {
-//     let element = document.getElementById("starRating_3");
-//     element.classList.add("fas");
-//     element.classList.add("fa-star");
-//     element.classList.add("bg-blue");
-// }
-// function goldenStarFour() {
-//     let element = document.getElementById("starRating_4");
-//     element.classList.add("fas");
-//     element.classList.add("fa-star");
-//     element.classList.add("bg-blue");
-// }
-//
-// function goldenStarFive() {
+TwoStar.addEventListener('mouseover', function () {
+    OneStar.classList.add("fas", "fa-star");
+    TwoStar.classList.add("fas", "fa-star");
+    ThreeStar.classList.remove("fas", "fa-star");
+    ThreeStar.classList.add("far", "fa-star");
+});
+
+ThreeStar.addEventListener('mouseover', function () {
+    OneStar.classList.add("fas", "fa-star");
+    TwoStar.classList.add("fas", "fa-star");
+    ThreeStar.classList.add("fas", "fa-star");
+    FourStar.classList.remove("fas", "fa-star");
+    FourStar.classList.add("far", "fa-star");
+});
+
+FourStar.addEventListener('mouseover', function () {
+    OneStar.classList.add("fas", "fa-star");
+    TwoStar.classList.add("fas", "fa-star");
+    ThreeStar.classList.add("fas", "fa-star");
+    FourStar.classList.add("fas", "fa-star");
+    FiveStar.classList.remove("fas", "fa-star");
+    FiveStar.classList.add("far", "fa-star");
+});
+
+FiveStar.addEventListener('mouseover', function () {
+    OneStar.classList.add("fas", "fa-star");
+    TwoStar.classList.add("fas", "fa-star");
+    ThreeStar.classList.add("fas", "fa-star");
+    FourStar.classList.add("fas", "fa-star");
+    FiveStar.classList.add("fas", "fa-star");
+});
+
+starBox.addEventListener('mouseleave', function() {
+    OneStar.classList.remove("fas", "fa-star");
+    OneStar.classList.add("far", "fa-star");
+
+    TwoStar.classList.remove("fas", "fa-star");
+    TwoStar.classList.add("far", "fa-star");
+
+    ThreeStar.classList.remove("fas", "fa-star");
+    ThreeStar.classList.add("far", "fa-star");
+
+    FourStar.classList.remove("fas", "fa-star");
+    FourStar.classList.add("far", "fa-star");
+
+    FiveStar.classList.remove("fas", "fa-star");
+    FiveStar.classList.add("far", "fa-star");
+});
+
+
+
+
+
+
+
+
+
+// function emptyStar() {
 //     let element = document.getElementById("starRating_5");
-//     element.classList.add("fas");
-//     element.classList.add("fa-star");
-//     element.classList.add("bg-blue");
+//     element.classList.add("far", "fa-star");
+//
 // }
 //
-// function emptyStarOne() {
-//     let element = document.getElementById("starRating_1");
-//     element.classList.add("far");
-//     element.classList.add("fa-star");
-//     element.classList.add("bg-blue");
-// }
-//
-// function emptyStarTwo() {
-//     let element = document.getElementById("starRating_2");
-//     element.classList.add("far");
-//     element.classList.add("fa-star");
-//     element.classList.add("bg-blue");
-// }
-//
-// function emptyStarThree() {
-//     let element = document.getElementById("starRating_3");
-//     element.classList.add("far");
-//     element.classList.add("fa-star");
-//     element.classList.add("bg-blue");
-// }
-//
-// function emptyStarFour() {
-//     let element = document.getElementById("starRating_4");
-//     element.classList.add("far");
-//     element.classList.add("fa-star");
-//     element.classList.add("bg-blue");
-// }
-//
-// function emptyStarFive() {
+// function fullstar() {
 //     let element = document.getElementById("starRating_5");
-//     element.classList.add("far");
-//     element.classList.add("fa-star");
-//     element.classList.add("bg-blue");
+//     element.classList.add("fas", "fa-star");
+
+// }
 
 
-// myFunc();
 
-// let oneStar = document.getElementById('starRating_1');
-// const TwoStar = document.getElementById('starRating_2');
-// const ThreeStar = document.getElementById('starRating_3');
-// const FourStar = document.getElementById('starRating_4');
-// const FiveStar = document.getElementById('starRating_5');
-//
-//
-//
-// // One star
-// oneStar.addEventListener('mouseover', function () {
-//     hoverStarOne();
-//     console.log("Yes");
-//
-// });
-//
-// oneStar.addEventListener('mouseout', function () {
-//     removeStarOne();
-//     console.log("Yes");
-//
-// });
-//
-// // Two Star
-// TwoStar.addEventListener('mouseover', function () {
-//     hoverStarOne();
-//     hoverStarTwo();
-//     console.log("Yes");
-//
-// });
-//
-// TwoStar.addEventListener('mouseout', function () {
-//     removeStarOne();
-//     removeStarTwo();
-//     console.log("Yes");
-//
-// });
-//
-//
-// // Three Star
-// ThreeStar.addEventListener('mouseover', function () {
-//     hoverStarOne();
-//     hoverStarTwo();
-//     hoverStarThree();
-//     console.log("Yes");
-//
-// });
-//
-// ThreeStar.addEventListener('mouseout', function () {
-//     removeStarOne();
-//     removeStarTwo();
-//     removeStarThree();
-//     console.log("Yes");
-//
-// });
-//
-// // Four Star
-// FourStar.addEventListener('mouseover', function () {
-//     hoverStarOne();
-//     hoverStarTwo();
-//     hoverStarThree();
-//     hoverStarFour();
-//     console.log("Yes");
-//
-// });
-//
-// FourStar.addEventListener('mouseout', function () {
-//     removeStarOne();
-//     removeStarTwo();
-//     removeStarThree();
-//     removeStarFour();
-//     console.log("Yes");
-//
-// });
-//
-// // Five Star
-// FiveStar.addEventListener('mouseover', function () {
-//     hoverStarOne();
-//     hoverStarTwo();
-//     hoverStarThree();
-//     hoverStarFour();
-//     hoverStarFive()
-//     console.log("Yes");
-//
-// });
-//
-// FiveStar.addEventListener('mouseout', function () {
-//     removeStarOne();
-//     removeStarTwo();
-//     removeStarThree();
-//     removeStarFour();
-//     removeStarFive()
-//     console.log("Yes");
-//
-// });
-//
-// function hoverStarOne() {
-//     let element = document.getElementById("starRating_1");
-//     element.classList.add("bg-black");
-// }
-//
-// function hoverStarTwo() {
-//     let element = document.getElementById("starRating_2");
-//     element.classList.add("bg-black");
-// }
-//
-// function hoverStarThree() {
-//     let element = document.getElementById("starRating_3");
-//     element.classList.add("bg-black");
-// }
-//
-// function hoverStarFour() {
-//     let element = document.getElementById("starRating_4");
-//     element.classList.add("bg-black");
-// }
-//
-// function hoverStarFive() {
-//     let element = document.getElementById("starRating_5");
-//     element.classList.add("bg-black");
-// }
-//
-// function removeStarOne() {
-//     let element = document.getElementById("starRating_1");
-//     element.classList.remove("bg-black");
-// }
-// function removeStarTwo() {
-//     let element = document.getElementById("starRating_2");
-//     element.classList.remove("bg-black");
-// }
-//
-// function removeStarThree() {
-//     let element = document.getElementById("starRating_3");
-//     element.classList.remove("bg-black");
-// }
-//
-// function removeStarFour() {
-//     let element = document.getElementById("starRating_4");
-//     element.classList.remove("bg-black");
-// }
-//
-// function removeStarFive() {
-//     let element = document.getElementById("starRating_5");
-//     element.classList.remove("bg-black");
-// }
 
 // $().button('toggle');
 
