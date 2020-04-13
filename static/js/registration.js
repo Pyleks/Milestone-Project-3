2e526b7d2fa1a2937d42c5caa8210d62108d1694
Pyleks <aleksander.andersen@outlook.ie>
2020-04-13 21:43:59 +0100

    Added username length rule

--- static/js/registration.js
+++ static/js/registration.js
@@ -1,4 +1,6 @@
 const username = document.getElementById('username');
+const content = document.createTextNode(" has too few characters");
+const empty = document.createTextNode(" empty username");
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const userLabel = document.getElementById('userLabel');
@@ -6,15 +8,23 @@ const form = document.getElementById('form');
 
 $(document).ready(function() {
     ($("form").submit(function (e) {
-        if (username.value === '' || username.value == null) {
-            userLabel.classList.add("fas", "fa-times");
-            $('#formOverview').attr('action', '/register');
-
-        } else if(username.value.length < 3 || username.value.length > 12) {
-            userLabel.classList.add("fas", "fa-times");
-            $(".user-feedback").css("color", "red");
+        const empty = document.createTextNode(" Is empty");
+        let myUsername = $("#username").val();
+        if (myUsername.trim().length === 0) {
+            console.log(myUsername.trim().length);
+            userLabel.appendChild(empty);
             e.preventDefault()
+
+        }if(myUsername.length < 3 || myUsername.length > 12) {
+            const usernamelenght = document.createTextNode(" Username Min 3 and Max 12 Characters");
+            console.log(username);
+            userLabel.appendChild(usernamelenght);
+            e.preventDefault()
+
+        } else {
+            $('#formOverview').attr('action', '/register');
         }
+
     }));
 });
 
