ccbfda311c08fe36137ab799231b4086eacc3bf9
Pyleks <aleksander.andersen@outlook.ie>
2020-04-14 18:52:12 +0100

    Version 1 complete of registration validation

--- static/js/registration.js
+++ static/js/registration.js
@@ -1,19 +1,16 @@
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const emailLabel = document.getElementById('email-label');
-const password = document.getElementById('password');
-const userLabel = document.getElementById('userLabel');
-const passwordLabel = document.getElementById('passwordLabel');
-const confirmPassword = document.getElementById("confirm-password");
-const confirmLabel = document.getElementById("confirm-password-label");
-
 $(document).ready(function() {
     ($("form").submit(function (e) {
         let myUsername = $("#username").val();
         let myPassword = $("#password").val();
         let myEmail = $("#email").val();
         let myConfirm = $("#confirm-password").val();
+        const userLabel = document.getElementById('userLabel');
+        const emailLabel = document.getElementById('emailLabel');
+        const passwordLabel = document.getElementById('passwordLabel');
+        const confirmLabel = document.getElementById("confirm-password-label");
         const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
         if (myUsername.trim().length === 0 || myUsername.trim().length < 3) {
             console.log(myUsername.trim().length);
             userLabel.innerText = "Username is Empty or Too Short";
@@ -21,7 +18,7 @@ $(document).ready(function() {
             e.preventDefault()
 
         }if(myUsername.trim().length > 12) {
-            console.log(username);
+            // console.log(username);
             userLabel.innerText = "Username Max 12 Characters";
             userLabel.style.color = "red";
             e.preventDefault()
@@ -33,17 +30,16 @@ $(document).ready(function() {
 
 
         }if(myPassword.trim().length === 0 || myPassword.trim().length < 8) {
-            console.log(username);
+            // console.log(username);
             passwordLabel.innerText = "Password is too short, Minimum 8 characters";
             passwordLabel.style.color = "red";
             e.preventDefault()
 
         }if(myPassword !== myConfirm) {
-            console.log(username);
+            // console.log(username);
             confirmLabel.innerText = "Please make sure password matches";
             confirmLabel.style.color = "red";
             e.preventDefault()
-
 
 
         } else {
