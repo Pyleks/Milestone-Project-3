4b000c5ff68875266dee5d36f5f9c093f8be0ad7
Pyleks <aleksander.andersen@outlook.ie>
2020-04-14 19:27:00 +0100

    Added URL verification to pastry creation page

---
+++ static/js/createRecipe.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    ($("#submitForm").click(function (e) {
+        let pastryUrl = $("#pastry_url").val();
+        const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+
+        if(!!pattern.test(pastryUrl) !== true) {
+            e.preventDefault()
+        }
+
+    }));
+});
