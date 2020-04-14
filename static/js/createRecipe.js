9c0d61ef4948794a4603ee2c08804a1b0bd63110
Pyleks <aleksander.andersen@outlook.ie>
2020-04-14 21:04:43 +0100

    Added check string on all all inputs in create recipe

--- static/js/createRecipe.js
+++ static/js/createRecipe.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
     ($("#submitForm").click(function (e) {
         let pastryUrl = $("#pastry_url").val();
         let portions = $("#pastry_portions").val();
+        let pastryName = $("#pastry_name").val();
+        let callouText = $("#pastry_callout").val();
+        let Ingredients = $("#pastry_ingredients").val();
+        let howTo = $("#pastry_howTo").val();
+
         const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
             '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
             '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
@@ -12,7 +17,19 @@ $(document).ready(function() {
         if(!!pattern.test(pastryUrl) !== true) {
             e.preventDefault()
 
-        }if(isNaN(portions) === true) {
+        }if(isNaN(portions) === true || portions.trim().length === 0) {
+            e.preventDefault()
+
+        }if(pastryName.trim().length === 0) {
+            e.preventDefault()
+
+        }if(callouText.trim().length === 0) {
+            e.preventDefault()
+
+        }if(Ingredients.trim().length === 0) {
+            e.preventDefault()
+
+        }if(howTo.trim().length === 0) {
             e.preventDefault()
         }
 
