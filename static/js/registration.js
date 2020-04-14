3c217d260235fbeb802b73db2d3c775749b60286
Pyleks <aleksander.andersen@outlook.ie>
2020-04-14 14:21:13 +0100

    Added password rules to JS

--- static/js/registration.js
+++ static/js/registration.js
@@ -4,11 +4,15 @@ const empty = document.createTextNode(" empty username");
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const userLabel = document.getElementById('userLabel');
+const passwordLabel = document.getElementById('passwordLabel');
+const confirmPassword = document.getElementById("confirm-password");
 const form = document.getElementById('form');
 
 $(document).ready(function() {
     ($("form").submit(function (e) {
         let myUsername = $("#username").val();
+        let mypassword = $("#password").val();
+        let myConfirm = $("#confirm-password").val();
         if (myUsername.trim().length === 0 || myUsername.trim().length < 3) {
             console.log(myUsername.trim().length);
             userLabel.innerText = "Username is Empty or Too Short";
@@ -21,7 +25,17 @@ $(document).ready(function() {
             userLabel.style.color = "red";
             e.preventDefault()
 
+        }if(mypassword.trim().length === 0 || mypassword.trim().length < 8) {
+            console.log(username);
+            passwordLabel.innerText = "Password too short, Minimum 8 characters";
+            passwordLabel.style.color = "red";
+            e.preventDefault()
 
+        }if(mypassword !== myConfirm) {
+            console.log(username);
+            passwordLabel.innerText = "Please make sure confirm password matches";
+            passwordLabel.style.color = "red";
+            e.preventDefault()
 
 
 
