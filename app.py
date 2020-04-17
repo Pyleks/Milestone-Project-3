8b305fc68e261525c56b65481b8e951a794aeb13
Pyleks <aleksander.andersen@outlook.ie>
2020-04-17 18:30:21 +0100

    Removed hardcoded port

--- app.py
+++ app.py
@@ -337,5 +337,5 @@ def logout():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT', '5005')),
+            port=int(os.environ.get('PORT')),
             debug=True)
