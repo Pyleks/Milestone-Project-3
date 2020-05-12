d55762b19f0765eff92789985533c898c47d1ed8
Pyleks <aleksander.andersen@outlook.ie>
2020-05-12 07:55:19 +0100

    Changed DB environment

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if path.exists("env.py"):
 
 # Connection to Database
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'TheHappyBun'
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
