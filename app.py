5810bded22bd913dafeff38e1381757b44dc3291
Pyleks <aleksander.andersen@outlook.ie>
2020-05-13 07:30:33 +0100

    Added 404 page

--- app.py
+++ app.py
@@ -450,6 +450,11 @@ def logout():
     return redirect(url_for('recipes'))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
