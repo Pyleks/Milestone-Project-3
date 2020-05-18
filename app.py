50a7ddd7c9662955eaaed3c3d0a06a83994b1cab
Pyleks <aleksander.andersen@outlook.ie>
2020-05-18 11:48:40 +0100

    Turned debug off

--- app.py
+++ app.py
@@ -452,4 +452,4 @@ def page_not_found(e):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
-            debug=True)
+            debug=False)
