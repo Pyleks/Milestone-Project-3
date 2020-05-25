6bdfdb4ac6af0e5b65ae23012f6015b5bf85b3f9
Pyleks <aleksander.andersen@outlook.ie>
2020-05-25 19:46:01 +0100

    Added if statement for star rating so it would not also toss error

--- static/js/createRecipe.js
+++ static/js/createRecipe.js
@@ -3,83 +3,83 @@ $(document).ready(function() {
     // Also makes sure it don't throw errors when opening other pages
     let recipeLoad = document.getElementById("recipe");
     if(recipeLoad) {
-    CKEDITOR.replace( 'recipe_ingredients', {
-        uiColor: '#6499ed',
-        customConfig: '/../static/js/config.js'
-    });
+        CKEDITOR.replace( 'recipe_ingredients', {
+            uiColor: '#6499ed',
+            customConfig: '/../static/js/config.js'
+        });
 
-    CKEDITOR.replace( 'recipe_howTo', {
-        uiColor: '#6499ed',
-        customConfig: '/../static/js/config.js'
-    });
-    }
-
-    // This JS is for making sure that recipe creation go well, and provide the client with sufficient feedback
-    // All the variables required to handle the operation
-    ($("#submitForm").click(function (e) {
-        let recipeUrl = $("#imageUrl").val();
-        let portions = $("#recipe_portions").val();
-        let recipeName = $("#recipe_name").val();
-        let calloutText = $("#recipe_callout").val();
-        const urlLabel = document.getElementById('urlLabel');
-        const recipeNameLabel = document.getElementById('recipeNameLabel');
-        const calloutLabel = document.getElementById('calloutLabel');
-        const portionLabel = document.getElementById('portionLabel');
+        CKEDITOR.replace( 'recipe_howTo', {
+            uiColor: '#6499ed',
+            customConfig: '/../static/js/config.js'
+        });
 
 
-            // This one is to make sure the URL string is correct
-        const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        // This JS is for making sure that recipe creation go well, and provide the client with sufficient feedback
+        // All the variables required to handle the operation
+        ($("#submitForm").click(function (e) {
+            let recipeUrl = $("#imageUrl").val();
+            let portions = $("#recipe_portions").val();
+            let recipeName = $("#recipe_name").val();
+            let calloutText = $("#recipe_callout").val();
+            const urlLabel = document.getElementById('urlLabel');
+            const recipeNameLabel = document.getElementById('recipeNameLabel');
+            const calloutLabel = document.getElementById('calloutLabel');
+            const portionLabel = document.getElementById('portionLabel');
 
-            // Checks if the URL is invalid
-        if(!!pattern.test(recipeUrl) !== true) {
-            urlLabel.innerText = "Please add a valid URL";
-            urlLabel.style.color = "red";
-            e.preventDefault();
 
-            // If the URL is valid, it provides valid URL
-        }if(!!pattern.test(recipeUrl) === true) {
-            urlLabel.innerText = "URL Valid";
-            urlLabel.style.color = "green";
+                // This one is to make sure the URL string is correct
+            const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+                '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+                '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+                '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
 
-            // Check if there is text and not just spaces, and if it's shorter then 3 characters
-        }if(recipeName.trim().length === 0 || recipeName.trim().length < 3) {
-            recipeNameLabel.innerText = "Recipe name empty or too short";
-            recipeNameLabel.style.color = "red";
-            e.preventDefault();
+                // Checks if the URL is invalid
+            if(!!pattern.test(recipeUrl) !== true) {
+                urlLabel.innerText = "Please add a valid URL";
+                urlLabel.style.color = "red";
+                e.preventDefault();
 
-            // Check if the recipe name is long enough
-        }if(recipeName.trim().length >= 3) {
-            recipeNameLabel.innerText = "Recipe Name Correct";
-            recipeNameLabel.style.color = "green";
+                // If the URL is valid, it provides valid URL
+            }if(!!pattern.test(recipeUrl) === true) {
+                urlLabel.innerText = "URL Valid";
+                urlLabel.style.color = "green";
 
-            // Check if call out text is just spaces or less then 5
-        }if(calloutText.trim().length === 0 || calloutText.trim().length < 5) {
-            calloutLabel.innerText = "Callout Text too short";
-            calloutLabel.style.color = "red";
-            e.preventDefault();
+                // Check if there is text and not just spaces, and if it's shorter then 3 characters
+            }if(recipeName.trim().length === 0 || recipeName.trim().length < 3) {
+                recipeNameLabel.innerText = "Recipe name empty or too short";
+                recipeNameLabel.style.color = "red";
+                e.preventDefault();
 
-            // If it's longer then 5 characters
-        }if(calloutText.trim().length > 5) {
-            calloutLabel.innerText = "Callout Text Correct";
-            calloutLabel.style.color = "green";
+                // Check if the recipe name is long enough
+            }if(recipeName.trim().length >= 3) {
+                recipeNameLabel.innerText = "Recipe Name Correct";
+                recipeNameLabel.style.color = "green";
 
-            // If the portions are not a number or just empty space
-        }if(isNaN(portions) === true || portions.trim().length === 0) {
-            portionLabel.innerText = "Please use numbers only";
-            portionLabel.style.color = "red";
-            e.preventDefault();
+                // Check if call out text is just spaces or less then 5
+            }if(calloutText.trim().length === 0 || calloutText.trim().length < 5) {
+                calloutLabel.innerText = "Callout Text too short";
+                calloutLabel.style.color = "red";
+                e.preventDefault();
 
-            // If NaN is false, it passes
-        }if(isNaN(portions) === false && portions.trim().length >= 1) {
-            portionLabel.innerText = "Number Correct";
-            portionLabel.style.color = "green";
+                // If it's longer then 5 characters
+            }if(calloutText.trim().length > 5) {
+                calloutLabel.innerText = "Callout Text Correct";
+                calloutLabel.style.color = "green";
 
-        }
+                // If the portions are not a number or just empty space
+            }if(isNaN(portions) === true || portions.trim().length === 0) {
+                portionLabel.innerText = "Please use numbers only";
+                portionLabel.style.color = "red";
+                e.preventDefault();
 
-    }));
-});
+                // If NaN is false, it passes
+            }if(isNaN(portions) === false && portions.trim().length >= 1) {
+                portionLabel.innerText = "Number Correct";
+                portionLabel.style.color = "green";
+
+            }
+
+        }));
+}});
--- static/js/home.js
+++ static/js/home.js
@@ -10,75 +10,77 @@ $(document).ready(function() {
             document.getElementById("overlay").style.display = "none";
         });
 
-        // Acquire all the variables for the star review to apply/remove stars.
-        let OneStar = document.getElementById('starRating_1');
-        const TwoStar = document.getElementById('starRating_2');
-        const ThreeStar = document.getElementById('starRating_3');
-        const FourStar = document.getElementById('starRating_4');
-        const FiveStar = document.getElementById('starRating_5');
-        const starBox = document.getElementById('star-box');
+        let recipeLoad = document.getElementById("find-overlay");
+        if(recipeLoad) {
+            // Acquire all the variables for the star review to apply/remove stars.
+            let OneStar = document.getElementById('starRating_1');
+            const TwoStar = document.getElementById('starRating_2');
+            const ThreeStar = document.getElementById('starRating_3');
+            const FourStar = document.getElementById('starRating_4');
+            const FiveStar = document.getElementById('starRating_5');
+            const starBox = document.getElementById('star-box');
 
-        // Star Review interactivity script, when hoovering over the stars, it will add all the stars up until
-        // The point you hoover, so hoover over star 3 it will populate star 1, 2 and 3.
-        // Adds stars as you hover over each.
-        OneStar.addEventListener('mouseover', function () {
-            OneStar.classList.add("fas", "fa-star");
-            TwoStar.classList.remove("fas", "fa-star");
-            TwoStar.classList.add("far", "fa-star");
+            // Star Review interactivity script, when hoovering over the stars, it will add all the stars up until
+            // The point you hoover, so hoover over star 3 it will populate star 1, 2 and 3.
+            // Adds stars as you hover over each.
+            OneStar.addEventListener('mouseover', function () {
+                OneStar.classList.add("fas", "fa-star");
+                TwoStar.classList.remove("fas", "fa-star");
+                TwoStar.classList.add("far", "fa-star");
+            });
+
+            // Adds stars as you hover over each.
+            TwoStar.addEventListener('mouseover', function () {
+                OneStar.classList.add("fas", "fa-star");
+                TwoStar.classList.add("fas", "fa-star");
+                ThreeStar.classList.remove("fas", "fa-star");
+                ThreeStar.classList.add("far", "fa-star");
+            });
+
+            // Adds stars as you hover over each.
+            ThreeStar.addEventListener('mouseover', function () {
+                OneStar.classList.add("fas", "fa-star");
+                TwoStar.classList.add("fas", "fa-star");
+                ThreeStar.classList.add("fas", "fa-star");
+                FourStar.classList.remove("fas", "fa-star");
+                FourStar.classList.add("far", "fa-star");
+            });
+
+            // Adds stars as you hover over each.
+            FourStar.addEventListener('mouseover', function () {
+                OneStar.classList.add("fas", "fa-star");
+                TwoStar.classList.add("fas", "fa-star");
+                ThreeStar.classList.add("fas", "fa-star");
+                FourStar.classList.add("fas", "fa-star");
+                FiveStar.classList.remove("fas", "fa-star");
+                FiveStar.classList.add("far", "fa-star");
+            });
+
+            // Adds stars as you hover over each.
+            FiveStar.addEventListener('mouseover', function () {
+                OneStar.classList.add("fas", "fa-star");
+                TwoStar.classList.add("fas", "fa-star");
+                ThreeStar.classList.add("fas", "fa-star");
+                FourStar.classList.add("fas", "fa-star");
+                FiveStar.classList.add("fas", "fa-star");
+            });
+
+            // A smart little trick to make the stars fully interactive by removing all stars, by taking the mouse
+            // out it clears all stars.
+            starBox.addEventListener('mouseleave', function () {
+                OneStar.classList.remove("fas", "fa-star");
+                OneStar.classList.add("far", "fa-star");
+
+                TwoStar.classList.remove("fas", "fa-star");
+                TwoStar.classList.add("far", "fa-star");
+
+                ThreeStar.classList.remove("fas", "fa-star");
+                ThreeStar.classList.add("far", "fa-star");
+
+                FourStar.classList.remove("fas", "fa-star");
+                FourStar.classList.add("far", "fa-star");
+
+                FiveStar.classList.remove("fas", "fa-star");
+                FiveStar.classList.add("far", "fa-star");
         });
-
-        // Adds stars as you hover over each.
-        TwoStar.addEventListener('mouseover', function () {
-            OneStar.classList.add("fas", "fa-star");
-            TwoStar.classList.add("fas", "fa-star");
-            ThreeStar.classList.remove("fas", "fa-star");
-            ThreeStar.classList.add("far", "fa-star");
-        });
-
-        // Adds stars as you hover over each.
-        ThreeStar.addEventListener('mouseover', function () {
-            OneStar.classList.add("fas", "fa-star");
-            TwoStar.classList.add("fas", "fa-star");
-            ThreeStar.classList.add("fas", "fa-star");
-            FourStar.classList.remove("fas", "fa-star");
-            FourStar.classList.add("far", "fa-star");
-        });
-
-        // Adds stars as you hover over each.
-        FourStar.addEventListener('mouseover', function () {
-            OneStar.classList.add("fas", "fa-star");
-            TwoStar.classList.add("fas", "fa-star");
-            ThreeStar.classList.add("fas", "fa-star");
-            FourStar.classList.add("fas", "fa-star");
-            FiveStar.classList.remove("fas", "fa-star");
-            FiveStar.classList.add("far", "fa-star");
-        });
-
-        // Adds stars as you hover over each.
-        FiveStar.addEventListener('mouseover', function () {
-            OneStar.classList.add("fas", "fa-star");
-            TwoStar.classList.add("fas", "fa-star");
-            ThreeStar.classList.add("fas", "fa-star");
-            FourStar.classList.add("fas", "fa-star");
-            FiveStar.classList.add("fas", "fa-star");
-        });
-
-        // A smart little trick to make the stars fully interactive by removing all stars, by taking the mouse
-        // out it clears all stars.
-        starBox.addEventListener('mouseleave', function () {
-            OneStar.classList.remove("fas", "fa-star");
-            OneStar.classList.add("far", "fa-star");
-
-            TwoStar.classList.remove("fas", "fa-star");
-            TwoStar.classList.add("far", "fa-star");
-
-            ThreeStar.classList.remove("fas", "fa-star");
-            ThreeStar.classList.add("far", "fa-star");
-
-            FourStar.classList.remove("fas", "fa-star");
-            FourStar.classList.add("far", "fa-star");
-
-            FiveStar.classList.remove("fas", "fa-star");
-            FiveStar.classList.add("far", "fa-star");
-    });
-}});
+}}});
