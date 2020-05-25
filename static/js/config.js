cd16d32d4006e5ba8adfc4c932bdeb0e95fa99e0
Pyleks <aleksander.andersen@outlook.ie>
2020-05-25 19:18:39 +0100

    Added comments to CKeditor

--- static/js/config.js
+++ static/js/config.js
@@ -1,9 +1,12 @@
+// Required to load the CKeditor configuration correctly
 CKEDITOR.editorConfig = function( config ) {
+    // Removes auto paragraph
     config.autoParagraph = false;
+    // Removes part of the toolbar in CKeditor
     config.toolbarGroups = [
         { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
     ];
-
+    // Removes remaining of the toolbar in CKeditor
     config.removeButtons = 'Underline,Italic,Bold,Strike,Subscript,Superscript,RemoveFormat';
     config.removePlugins = 'elementspath,resize';
 
